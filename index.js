9100b075fcaf1a69c97564c5c80c44fe1d21e3df
Josh Kuhar <jdkuhar@gmail.com>
2016-09-20 19:03:35 -0400

    update

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 //var greeting = require('./abc');
-var stream = require('stream');
-var fs = require('fs');
+//var stream = require('stream');
+// var fs = require('fs');
 
-var inFile = fs.createReadStream('abc.js');
-inFile.on('data', function(chunk) {
-	console.log(stream.Readable.read);
-    console.log(chunk.toString());
-});
-	
+// var inFile = fs.createReadStream('abc.js');
+// inFile.on('data', function(chunk) {
+// 	console.log();
+//     console.log(chunk.toString());
+// });
+// 	
+
+var Readable = require('stream').Readable;
+var rs = new Readable;
+rs.push('beep ');
+rs.push('boop\n');
+rs.push(null);
+
+rs.pipe(process.stdout);
